9fa7dc1f01520f6a856b5ae73c4cb1a3cfae6e5d
Uluc Sengil <usengil@ku.edu.tr>
2016-02-24 17:38:40 +0200

    styling completed with RegEx

--- fromBottom.py
+++ fromBottom.py
@@ -12,21 +12,28 @@ onlyfiles = [join(path, f) for f in listdir(path) if isfile(join(path, f)) and f
 print onlyfiles
 
 for x in onlyfiles:
+	
 	process = Popen(["TextBox", x], stdout=PIPE)
 	(output, err) = process.communicate()
 	exit_code = process.wait()
 	stripped = output.strip()
-	if stripped == "The page does not contain answers" or stripped == "Cannot open source image!":
+
+	if stripped == "The page does not contain answers" or stripped == "Cannot open source image!" or stripped == "":
 		continue
 	else:
 		stripped = stripped.upper()
 		offset += 1
 		if stripped[0] != "1":
 			stripped = stripped[1:]
+		# pure stylistic changes to fit the format
+		# regex helps us to preserve the generalness
+		# and in this situation speed doesn't matter as much
 		stripped = re.sub(" ", "\t", stripped)
 		stripped = re.sub("[-\xe2]", ".", stripped)
+		stripped = re.sub("^\.","1.",stripped)
 		stripped = re.sub("\.", ". ", stripped)
-		stripped = re.sub("[^A-E0-9\. \s\t]","",stripped)
+		stripped = re.sub("[^A-ETS0-9\. \s\t]","",stripped)
+		stripped = re.sub("\t(?=[0-9]1|[1-9]6|6)","\n",stripped)
 
 		print >>f1, "TEST - %d" % (offset)
 		print >>f1, stripped
--- fromEnd.py
+++ fromEnd.py
@@ -12,18 +12,29 @@ onlyfiles = [join(path, f) for f in listdir(path) if isfile(join(path, f)) and f
 print onlyfiles
 
 for x in onlyfiles:
+
 	process = Popen(["tesseract", x, "stdout", "-psm", "6", "-l", "tst"], stdout=PIPE)
 	(output, err) = process.communicate()
 	exit_code = process.wait()
 	stripped = output.strip()
-	if stripped == "The page does not contain answers" or stripped == "Cannot open source image!":
+
+	if stripped == "The page does not contain answers" or stripped == "Cannot open source image!" or stripped == "":
 		continue
 	else:
 		stripped = stripped.upper()
 		offset += 1
 		if stripped[0] != "1":
 			stripped = stripped[1:]
+		# pure stylistic changes to fit the format
+		# regex helps us to preserve the generalness
+		# and in this situation speed doesn't matter as much
 		stripped = re.sub("\n\n", "\n", stripped)
+		stripped = re.sub(" ", "\t", stripped)
+		stripped = re.sub("[-\xe2]", ".", stripped)
+		stripped = re.sub("\.", ". ", stripped)
+		stripped = re.sub("[^A-ETS0-9\. \s\t]","",stripped)
+		stripped = re.sub("T\.","T -", stripped)
+		stripped = re.sub(r"\bEST","TEST", stripped)
 
 	print >>f1, stripped
 	print "Page %d imported" % (offset)
