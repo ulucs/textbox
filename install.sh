351b7f2932a0dcdfdd54d56127465a52409ee0d5
Uluc Sengil <usengil@ku.edu.tr>
2016-03-02 16:41:47 +0200

    folder restructure and boxslice.cpp

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo CMake .
-sudo make
-sudo cp TextBox /usr/local/bin
-sudo cp fromBottom.py fromEnd.py /usr/local/lib
+pushd src/textbox/build; cmake ..; make; popd
+pushd src/boxslice/build; cmake ..; make; popd
+sudo cp src/textbox/build/TextBox /usr/local/bin
+sudo cp src/boxslice/build/boxslice /usr/local/bin
