14977946d7433c71e16b0f407dbf3f8774c2981f
ulucs <usengil@ku.edu.tr>
2016-03-10 18:47:01 +0200

    fixed some viewing issues

--- src/selectText/selecttext.cpp
+++ src/selectText/selecttext.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv.hpp"
 #include <iostream>
 #include <tesseract/baseapi.h>
 
@@ -9,7 +10,9 @@ using namespace std;
 
 bool leftDown=false,leftup=false;
 Mat img;
+Mat viewimg;
 Mat crop;
+double shrinkBy;
 Point cor1,cor2;
 Rect box;
 
@@ -43,20 +46,31 @@ void mouse_call(int event,int x,int y,int,void*)
 		Point pt;
 		pt.x=x;
 		pt.y=y;
-		Mat temp_img=img.clone();
+		Mat temp_img=viewimg.clone();
 		rectangle(temp_img,cor1,pt,Scalar(0,0,255)); //drawing a rectangle continuously
 		imshow("Original",temp_img);
 	}
 
 	if(leftDown==true&&leftup==true) //when the selection is done
 	{
-		box.width=abs(cor1.x-cor2.x);
-		box.height=abs(cor1.y-cor2.y);
-		box.x=min(cor1.x,cor2.x);
-		box.y=min(cor1.y,cor2.y);
+		box.width=abs(cor1.x-cor2.x) / shrinkBy;
+		box.height=abs(cor1.y-cor2.y) / shrinkBy;
+		box.x=min(cor1.x,cor2.x) / shrinkBy;
+		box.y=min(cor1.y,cor2.y) / shrinkBy;
 		crop = img(box);   //Selecting a ROI(region of interest) from the original pic
+
+		Mat cropShow;
+		int viewHeight1 = 700;
+		if (crop.rows > viewHeight1)
+		{
+			double csh = (double) viewHeight1 / crop.rows;
+			resize(crop, cropShow, Size(), csh, csh);
+		}
+		else
+			cropShow = crop.clone();
+
 		namedWindow("Cropped Image");
-		imshow("Cropped Image",crop); //showing the cropped image
+		imshow("Cropped Image",cropShow); //showing the cropped image
 		leftDown=false;
 		leftup=false;
 	}
@@ -78,22 +92,33 @@ int main(int argc,char** argv)
 		return -1;
 	}
 
-	namedWindow("Original");
-	imshow("Original",img);
+	int viewHeight = 760;
+	shrinkBy = (double) viewHeight / img.rows;
+
+	resize(img, viewimg, Size(), shrinkBy, shrinkBy);
+
+	namedWindow("Original", 0);
+	imshow("Original",viewimg);
 
 	setMouseCallback("Original",mouse_call); //setting the mouse callback for selecting the region with mouse
 
-	while(char(waitKey(1)!='n')) //waiting for the 'q' key to finish the execution
-	{
+	waitKey(0);
 
-	}
+	destroyAllWindows();
 
-	threshold(crop,crop,150,255,THRESH_BINARY_INV);
+	fastNlMeansDenoising(crop,crop,17);
 	cvtColor( crop, crop, CV_BGR2GRAY );
+	// adaptiveThreshold(crop,crop,255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 25, 0);
+
+	// imshow("Modded", crop);
+	// while(char(waitKey(1)!='n')) //waiting for the 'q' key to finish the execution
+	// {
+
+	// }
 
 	// pass the image to tesseract
 	tesseract::TessBaseAPI tess;
-	tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
+	tess.Init(NULL, "tst", tesseract::OEM_DEFAULT);
 	tess.SetImage((uchar*)crop.data, crop.cols, crop.rows, 1, crop.cols);
 
 	// print the characters of the image
