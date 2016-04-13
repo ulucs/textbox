b253c2e7ececaf6815d725f8a37fa333f5e188f1
Uluc Sengil <usengil@ku.edu.tr>
2016-04-13 10:51:03 +0300

    fromEnd trivial changes

--- tools/fromEnd.py
+++ tools/fromEnd.py
@@ -4,7 +4,7 @@ from os.path import isfile, join
 import re
 import sys
 
-path = "testdocs/fdd"
+path = sys.argv[1]
 offset = 0
 
 f1 = open(join(path,"cevaplar.txt"), 'w+')
@@ -14,7 +14,7 @@ print onlyfiles
 
 for x in onlyfiles:
 
-	process = Popen(["/usr/local/bin/tesseract", x, "stdout", "-psm", "6", "-l", "tst"], stdout=PIPE)
+	process = Popen(["/usr/local/bin/tesseract", x, "stdout", "-psm", "3", "-l", "tst"], stdout=PIPE)
 	(output, err) = process.communicate()
 	exit_code = process.wait()
 	stripped = output.strip()
@@ -24,28 +24,28 @@ for x in onlyfiles:
 	else:
 		stripped = stripped.upper()
 		offset += 1
-		if stripped[0] != "1":
-			stripped = stripped[1:]
-		# pure stylistic changes to fit the format
-		# regex helps us to preserve the generalness
-		# and in this situation speed doesn't matter as much
-		stripped = re.sub("\n\n", "\n", stripped)
-		stripped = re.sub(" ", "\t", stripped)
-		stripped = re.sub("[-\xe2]", ".", stripped)
-		stripped = re.sub("\.", ". ", stripped)
-		stripped = re.sub("[^A-ETS0-9\. \s\t]","",stripped)
-		# fix the misread (T)EST
-		stripped = re.sub("T\.","T -", stripped)
-		stripped = re.sub(r"\bEST","TEST", stripped)
-		# clean misadded tabs
-		stripped = re.sub(r"\t(?=[A-ES])","", stripped)
-		# sinav clean SECENEKLER, add test numbers if not provided
-		stripped = re.sub("SE+\n","", stripped)
-		if stripped[0] == "1":
-			stripped = "TEST - num0\n" + stripped
-		# adaptation for any length columns
-		stripped = re.sub("\n(?!1\.|T)","\t", stripped)
-		stripped = re.sub("\t(?=[0-9](6|1)|6)","\n", stripped)
+		# if stripped[0] != "1":
+		# 	stripped = stripped[1:]
+		# # pure stylistic changes to fit the format
+		# # regex helps us to preserve the generalness
+		# # and in this situation speed doesn't matter as much
+		# stripped = re.sub("\n\n", "\n", stripped)
+		# stripped = re.sub(" ", "\t", stripped)
+		# stripped = re.sub("[-\xe2]", ".", stripped)
+		# stripped = re.sub("\.", ". ", stripped)
+		# stripped = re.sub("[^A-ETS0-9\. \s\t]","",stripped)
+		# # fix the misread (T)EST
+		# stripped = re.sub("T\.","T -", stripped)
+		# stripped = re.sub(r"\bEST","TEST", stripped)
+		# # clean misadded tabs
+		# stripped = re.sub(r"\t(?=[A-ES])","", stripped)
+		# # sinav clean SECENEKLER, add test numbers if not provided
+		# stripped = re.sub("SE+\n","", stripped)
+		# if stripped[0] == "1":
+		# 	stripped = "TEST - num0\n" + stripped
+		# # adaptation for any length columns
+		# stripped = re.sub("\n(?!1\.|T)","\t", stripped)
+		# stripped = re.sub("\t(?=[0-9](6|1)|6)","\n", stripped)
 
 	print >>f1, stripped
-	print "Page %d imported" % (offset)
+	print >>f1, "Page %d imported" % (offset)
