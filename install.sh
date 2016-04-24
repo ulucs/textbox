13a9873fbf73b6e9cf0653a6f18189440f64474d
Uluc Sengil <usengil@ku.edu.tr>
2016-04-24 17:52:00 +0300

    move binaries to bin folder so brew can do its work

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-mkdir src/textbox/build
+mkdir -p bin
+mkdir -p src/textbox/build
 pushd src/textbox/build; cmake ..; make; popd
-sudo cp src/textbox/build/TextBox /usr/local/bin
-# mkdir src/boxslice/build
+sudo cp src/textbox/build/TextBox bin
+# mkdir -p src/boxslice/build
 # pushd src/boxslice/build; cmake ..; make; popd
 # sudo cp src/boxslice/build/boxslice /usr/local/bin
-mkdir src/selecttext/build
+mkdir -p src/selecttext/build
 pushd src/selecttext/build; cmake ..; make; popd
-sudo cp src/selecttext/build/selecttext /usr/local/bin
-mkdir src/textbox2/build
+sudo cp src/selecttext/build/selecttext bin
+mkdir -p src/textbox2/build
 pushd src/textbox2/build; cmake ..; make; popd
-sudo cp src/textbox2/build/textbox2 /usr/local/bin
-mkdir src/layoutfit/build
+sudo cp src/textbox2/build/textbox2 bin
+mkdir -p src/layoutfit/build
 pushd src/layoutfit/build; cmake ..; make; popd
-sudo cp src/layoutfit/build/layoutfit /usr/local/bin
-mkdir src/layoutcrop/build
+sudo cp src/layoutfit/build/layoutfit bin
+mkdir -p src/layoutcrop/build
 pushd src/layoutcrop/build; cmake ..; make; popd
-sudo cp src/layoutcrop/build/layoutcrop /usr/local/bin
+sudo cp src/layoutcrop/build/layoutcrop bin
--- readme.md
+++ readme.md
@@ -2,14 +2,7 @@
 
 Just paste this into terminal
 
-	sudo chown -R $(whoami):admin /usr/local && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-	brew install homebrew/science/opencv --without-python && \
-	brew install tesseract && \
-	brew install cmake && \
-	brew install imagemagick --with-ghostscript && \
-	brew install ghostscript && \
-	cd Desktop && pushd ~ && git clone https://github.com/ulucs/textbox && pushd textbox && \
-	./install.sh && cp /tst/tst.traineddata /usr/local/Cellar/tesseract/3.04.01_1/share/tessdata/tst.traineddata && popd && popd
+	brew install ulucs/tap/textbox
 
 ## How to run
 
@@ -19,9 +12,3 @@ Just paste this into terminal
 4. If not, press any button to skip
 5. After selecting the area of interest, press any button to run
 6. For each pdf, the readings are in cevaplar.txt in the folder with the name of pdf
-
-## Pre-reqs
-
-1. Tesseract
-2. OpenCV
-3. Imagemagick
